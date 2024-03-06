35a070481d122b7bc652306780fdf2c500c7a369
Jakob Widauer <jakob.widauer@gmail.com>
2024-03-06 02:54:59 +0100

    feat: allow using multicast addresses for udp socket and join the multicast group automatically

--- src/endpoint/udp.rs
+++ src/endpoint/udp.rs
@@ -1,4 +1,5 @@
-use std::net::{SocketAddr, ToSocketAddrs};
+use log::debug;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
 use tokio::net::UdpSocket;
 
 pub struct UdpTransmitter {
@@ -7,7 +8,22 @@ pub struct UdpTransmitter {
 
 impl UdpTransmitter {
     pub fn new<A: ToSocketAddrs>(addr: A) -> Result<Self, std::io::Error> {
+        let addr = addr.to_socket_addrs()?.next().ok_or(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Could not resolve to any address",
+        ))?;
+
+        debug!("Binding UDP socket to {}", addr);
         let socket = std::net::UdpSocket::bind(addr)?;
+
+        // Join multicast group if address is multicast
+        if addr.ip().is_multicast() {
+            debug!("Joining multicast group {}", addr.ip());
+            match addr.ip() {
+                IpAddr::V4(a) => socket.join_multicast_v4(&a, &Ipv4Addr::UNSPECIFIED)?,
+                IpAddr::V6(a) => socket.join_multicast_v6(&a, 0)?,
+            }
+        }
         socket.set_nonblocking(true)?;
         let socket = UdpSocket::from_std(socket)?;
         Ok(Self { socket })
